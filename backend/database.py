1628ea21e8b0fd8306c354fb45080ef7908e09ba
oV1t <102901228+oV1t@users.noreply.github.com>
2026-03-25 22:22:19 +0200

    add Dokerfile(frontend and backend) and docker-compose

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, Session, SQLModel
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./elective_disciplines.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:PASSWORD@postgres:5432/elective_disciplines")
 
 engine = create_engine(
     DATABASE_URL, 
